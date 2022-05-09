9d0d033c8774157babcbb412f767ef24995860b4
Ike <ike12346@gmail.com>
2022-05-08 17:59:42 -0700

    Build out home page UI

--- src/controllers/home-page.controller.ts
+++ src/controllers/home-page.controller.ts
@@ -1,7 +1,5 @@
 import { RequestHandler } from 'express';
 
 export const home_page_index: RequestHandler = (_req, res) => {
-  res.render('home', {
-    title: 'Hello, World!'
-  });
+  res.render('home');
 };
